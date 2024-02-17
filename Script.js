5f4488125df2695a217fe4e36d314ac70f8c9929
UnityCraft <160357715+SiecUnityCraft@users.noreply.github.com>
2024-02-17 23:47:00 +0100

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,8 @@
+function discord() 
+{
+    window.location.href = 'https://discord.gg/24zG7SPmsn';
+}
+function youtube() 
+{
+    window.location.href = 'https://www.youtube.com/channel/UCvO6GKBeAyn0b32241rVniw';
+}
